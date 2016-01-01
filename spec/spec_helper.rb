130218193f4695850d6024fb69e848fff59d43a0
Richard Newman <richard@newmanworks.com>
2015-12-31 19:24:17 -0700

    add simplecov for local test coverage review

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,7 @@
 require "codeclimate-test-reporter"
+require 'simplecov'
 CodeClimate::TestReporter.start
+SimpleCov.start
 
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'loba'
