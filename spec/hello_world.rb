55b60b9fef8c2c561b9cd512b8666f8990bceb80
Richard Newman <richard@newmanworks.com>
2015-12-31 19:45:30 -0700

    refactor tests to start baseline real tests

---
+++ spec/hello_world.rb
@@ -0,0 +1,13 @@
+class HelloWorld
+  def initialize
+    @x = 42
+Loba::ts        # see? it's easier to see what to remove later
+    @y = "Charlie"
+  end
+
+  def hello
+Loba::val :@x
+    puts "Hello, #{@y}" if @x == 42
+Loba::ts
+  end
+end
--- spec/loba_spec.rb
+++ spec/loba_spec.rb
@@ -1,61 +1,143 @@
 require 'spec_helper'
-
-class LobaClass
-  include Loba
-
-  def base
-    Loba.ts
-    _v = "BENJAMIN"
-    Loba::ts
-    Loba::val :_v
-    Loba::val self.methods.sort.uniq
-  end
-
-  def self.classbase
-    Loba::ts
-    _w = "CLAUDIA"
-    Loba::ts
-    Loba::val :_w
-  end
-end
+require 'loba_class'
+require 'hello_world'
 
 describe Loba do
   it 'has a version number' do
     expect(Loba::VERSION).not_to be nil
   end
 
-  context 'ts (timestamp)' do
-    let (:test_class) do
-      Class.new(LobaClass) do
-        def hello
-          Loba.ts
-          v = "hello"
-          Loba::ts
-          Loba::val :v
-        #  ts  # should fail
-          v
+  context '[output suppressed]' do
+
+    before :all do
+      @original_stderr = $stderr
+      @original_stdout = $stdout
+      $stderr = File.open(File::NULL, "w")
+      $stdout = File.open(File::NULL, "w")
+    end
+
+    after :all do
+      $stderr = @original_stderr
+      $stdout = @original_stdout
+    end
+
+    describe 'ts (timestamp notice)' do
+
+      it 'can be called as Loba.ts' do
+        test_class = Class.new(LobaClass) do
+          def hello
+            Loba.ts
+          end
         end
+        expect{test_class.new.hello}.not_to raise_error
+      end
+
+      it 'can be called as Loba::ts' do
+        test_class = Class.new(LobaClass) do
+          def hello
+            Loba::ts
+          end
+        end
+        expect{test_class.new.hello}.not_to raise_error
+      end
+
+      it 'cannot be called as ts only' do
+        test_class = Class.new(LobaClass) do
+          def hello
+            ts
+          end
+        end
+        expect{test_class.new.hello}.to raise_error NameError
+      end
+
+      it 'can be called from instance methods' do
+        expect{LobaClass.new.base_ts}.not_to raise_error
+      end
+
+      it 'can be called from class methods' do
+        expect{LobaClass.classbase_ts}.not_to raise_error
+      end
+
+      it 'can write to STDOUT' do
+        test_class = Class.new(LobaClass) do
+          def hello
+            Loba.ts
+          end
+        end
+        expect{test_class.new.hello}.to output(/\[TIMESTAMP\]/).to_stdout
       end
     end
 
-    it 'can be called' do
-      puts "FOR HELLO"
-      expect{test_class.new.hello}.not_to raise_error
-    end
+    describe 'val (value notice)' do
 
-    it 'base can be called' do
-      puts "IN BASE"
-      expect{LobaClass.new.base}.not_to raise_error
-    end
+      it 'can be called as Loba.val' do
+        test_class = Class.new(LobaClass) do
+          def hello
+            _v = "hello"
+            Loba.val :_v
+          end
+        end
+        expect{test_class.new.hello}.not_to raise_error
+      end
 
-    it 'class base can be called' do
-      puts "IN CLASS BASE"
-      expect{LobaClass.classbase}.not_to raise_error
-    end
+      it 'can be called as Loba::val' do
+        test_class = Class.new(LobaClass) do
+          def hello
+            _v = "hello"
+            Loba::val :_v
+          end
+        end
+        expect{test_class.new.hello}.not_to raise_error
+      end
 
-    it 'can write to STDOUT' do
-      expect{test_class.new.hello}.to output(/\[TIMESTAMP\]/).to_stdout
+      it 'cannot be called as val only' do
+        test_class = Class.new(LobaClass) do
+          def hello
+            _v = "hello"
+            val :_v
+          end
+        end
+        expect{test_class.new.hello}.to raise_error NameError
+      end
+
+      it 'can be called from instance methods' do
+        expect{LobaClass.new.base_val}.not_to raise_error
+      end
+
+      it 'can be called from class methods' do
+        expect{LobaClass.classbase_val}.not_to raise_error
+      end
+
+      it 'can write to STDOUT' do
+        test_class = Class.new(LobaClass) do
+          def hello
+            Loba.ts
+          end
+        end
+        expect{test_class.new.hello}.to output(/\[TIMESTAMP\]/).to_stdout
+      end
     end
 
   end
+
+  context '[output allowed]' do
+    context 'in HelloWorld demo,' do
+      subject(:hello_world) { HelloWorld.new.hello }
+      it 'writes a first timestamp' do
+        expected_output = /\[TIMESTAMP\] #=.*, diff=.*, at=.*, in=.*.rb:4:in `initialize'/
+        expect{subject}.to output(expected_output).to_stdout
+      end
+
+      it 'writes a value notice' do
+        expected_output = /\[HelloWorld#hello\] @x: 42.*at .*.rb:9:in `hello'/
+        expect{subject}.to output(expected_output).to_stdout
+      end
+
+      it 'writes a second timestamp' do
+        expected_output = /\[TIMESTAMP\] #=.*, diff=.*, at=.*, in=.*.rb:11:in `hello'/
+        expect{subject}.to output(expected_output).to_stdout
+      end
+    end
+  end
+
 end
