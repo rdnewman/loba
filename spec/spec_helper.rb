5f23574e8fe1e71b35262a8db28bafa2cedc7980
Richard Newman <richard@newmanworks.com>
2016-01-02 16:20:54 -0700

    verify how calling signatures work

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,3 +5,8 @@ SimpleCov.start
 
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'loba'
+
+RSpec.configure do |config|
+  config.filter_run :focus
+  config.run_all_when_everything_filtered = true
+end
