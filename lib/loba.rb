63dce3c789e55b2e71e54ab5ff715eb0ae1711b0
Richard Newman <richard@newmanworks.com>
2016-01-02 16:34:12 -0700

    minor doc nit

--- lib/loba.rb
+++ lib/loba.rb
@@ -114,6 +114,10 @@ private
   end
 
   # Internal class for tracking time stamps; should not be used directly
+  # @!attribute [rw] timewas
+  #   Previous timestamped Time value
+  # @!attribute [rw] timenum
+  #   Count of timestamping occurances so far
   class TimeKeeper
     include Singleton
     attr_accessor :timewas, :timenum
