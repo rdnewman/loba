451fa4391ff7c48dcf48a5f036d5d91f207bab2d
Richard Newman <richard@newmanworks.com>
2021-07-18 12:27:53 -0600

    resolving code climate use by removing rubocop-performance requirement

--- Gemfile
+++ Gemfile
@@ -4,11 +4,11 @@ gemspec
 
 group :development do
   gem 'rubocop', require: false
-  gem 'rubocop-performance', require: false
   gem 'rubocop-rspec', require: false
   gem 'yard'
 end
 
 group :test do
   gem 'simplecov', require: false
+  gem 'codeclimate-test-reporter', require: nil
 end
--- spec/loba/hello_world.rb
+++ spec/loba/hello_world.rb
@@ -1,19 +1,19 @@
 class HelloWorld
   def initialize
     @x = 42
-    Loba.ts        # see? it's easier to see what to remove later
+Loba.ts # see? it's easier to see what to remove later
     @y = 'Charlie'
   end
 
   def hello
-    Loba.val :@x
+Loba.val :@x
     puts "Hello, #{@y}" if @x == 42
-    Loba.ts
+Loba.ts
   end
 
   def goodbye
-    Loba.val @y, '@y'
+Loba.val @y, '@y'
     puts "Goodbye, #{@y}" if @x == 42
-    Loba.ts
+Loba.ts
   end
 end
