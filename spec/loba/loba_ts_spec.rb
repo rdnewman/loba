bbd279fcb790190d51aab8dcc8853d42064bbc15
Richard Newman <rdnewman@users.noreply.github.com>
2021-08-20 13:22:26 -0600

    Migrate from colorize gem to rainbow gem for notice coloring (#31)
    * migrate to Rainbow gem from colorize
    * migrate changelog from README to use "Keep a Changelog" conventions
    * add test for ill-formed supplied labels
    * convert calls to use string continuation instead of concatenation; clean up old comments
    * stop rubocop from suggesting rubocop-rake
    * update CHANGELOG with new detail
    * prevent Travis-CI builds from failing when color output tests are involved (Travis may be filtering color from strings)
    * cache gem bundling in Travis

--- spec/loba/loba_ts_spec.rb
+++ spec/loba/loba_ts_spec.rb
@@ -81,4 +81,80 @@ RSpec.describe Loba, '.ts' do
       expect { LobaClass.new.base_ts }.to output(/, err=#{error_message}/).to_stdout
     end
   end
+
+  unless ENV['LOBA_SPEC_IN_TRAVIS']
+    describe 'colors text' do
+      let(:nocolor) { /\e\[0m/ }
+
+      context 'when successful' do
+        let(:nobg) { /\e\[49m/ } # default background
+
+        let(:bannercolor) { /\e\[30m/ } # black (foreground)
+        let(:bannerbg)    { /\e\[100m/ } # light black (background)
+        let(:bannertext)  { /\[TIMESTAMP\]/ } # [TIMESTAMP]
+        let(:bannerref)   { /#{bannercolor}#{bannerbg}#{bannertext}#{nocolor}/ }
+
+        let(:labelcolor) { /\e\[33m/ } # yellow (foreground)
+
+        let(:countlabel) { /#{labelcolor}#{nobg}\s#=#{nocolor}\d{4}/ }
+        let(:difflabel)  { /#{labelcolor},\sdiff=#{nocolor}\d+\.\d{6}/ }
+        let(:atlabel)    { /#{labelcolor},\sat=#{nocolor}\d+\.\d{6}/ }
+
+        let(:codecolor)  { /\e\[90m/ } # dim grey
+        let(:codespacer) { /\s*\t/ } # spaces, then tab
+        let(:coderef)    { /#{codecolor}#{codespacer}\(in\s.*\)#{nocolor}/ }
+
+        it 'when successful with various colors' do
+          test_object = LobaClass.new
+
+          # essentially the same as:
+          # "\e[30m\e[100m[TIMESTAMP]\e[0m" \
+          # "\e[33m\e[49m #=\e[0m" \
+          # '0007' \
+          # "\e[33m, diff=\e[0m" \
+          # '0.000578' \
+          # "\e[33m, at=\e[0m" \
+          # '1629403248.858656' \
+          # "\e[90m    \t" \
+          # '(in=/home/richard/src/loba/spec/loba/loba_class.rb:' \
+          # '3' \
+          # ":in `base_ts')" \
+          # "\e[0m\n"
+          colored_output = /#{bannerref}#{countlabel}#{difflabel}#{atlabel}#{coderef}\n/
+
+          expect { test_object.base_ts }.to output(colored_output).to_stdout
+        end
+      end
+
+      context 'upon error' do
+        let(:errorcolor) { /\e\[31m/ } # red
+
+        let(:banner)     { /\[TIMESTAMP\]/ }
+
+        let(:countlabel) { /#=FAIL/ }
+        let(:inlabel)    { /in=\/[\w\/]*\/lib\/loba\.rb:\d+:in\s`timestamp'/ }
+        let(:errlabel)   { /err=fake\serror/ }
+        let(:body)       { /#{countlabel},\s#{inlabel},\s#{errlabel}/ }
+
+        let(:error_message) { 'fake error' }
+
+        before do
+          allow(Loba::Internal::TimeKeeper.instance).to receive(:ping).and_raise(error_message)
+        end
+
+        it 'with red' do
+          # essentially the same as:
+          #   "\e[31m" \
+          #   '[TIMESTAMP] ' \
+          #   '#=FAIL, ' \
+          #   "in=/path/to/code/lib/loba.rb:33:in `timestamp'," \
+          #   'err=fake error' \
+          #   "\e[0m\n"
+          colored_output = /#{errorcolor}#{banner}\s#{body}#{nocolor}\n/
+
+          expect { LobaClass.new.base_ts }.to output(colored_output).to_stdout
+        end
+      end
+    end
+  end
 end
